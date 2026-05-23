a124d9acad4de0fd0e888493dc72caab5ba3cb58
chenxuecong2 <168695974+chenxuecong2@users.noreply.github.com>
2026-05-24 00:47:38 +0800

    Update and rename omni_X70pro.mk to twrp_X70pro.mk

--- twrp_X70pro.mk
+++ twrp_X70pro.mk
@@ -5,18 +5,19 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit from TWRP product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from X70pro device
 $(call inherit-product, device/5g/X70pro/device.mk)
 
 PRODUCT_DEVICE := X70pro
-PRODUCT_NAME := omni_X70pro
+PRODUCT_NAME := twrp_X70pro
 PRODUCT_BRAND := 5G
 PRODUCT_MODEL := X70pro
 PRODUCT_MANUFACTURER := 5g
